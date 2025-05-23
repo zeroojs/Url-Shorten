956ce4d8ad9932b1b6ca751b9085438f61c7fe8f
zeroojs <76021130+zeroojs@users.noreply.github.com>
2025-05-23 08:45:24 +0800

    Update main.js

--- main.js
+++ main.js
@@ -31,7 +31,7 @@ function shorturl() {
   }).then(function (myJson) {
     res = myJson;
     document.getElementById("addBtn").disabled = false;
-    document.getElementById("addBtn").innerHTML = 'Shorten it';
+    document.getElementById("addBtn").innerHTML = '提交';
 
     // 成功生成短链 Succeed
     if (res.status == "200") {
@@ -55,7 +55,7 @@ function shorturl() {
     alert("Unknow error. Please retry!");
     console.log(err);
     document.getElementById("addBtn").disabled = false;
-    document.getElementById("addBtn").innerHTML = 'Shorten it';
+    document.getElementById("addBtn").innerHTML = '提交';
   })
 }
 
